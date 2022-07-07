1d1c75e91af3d8a69dc33d047c0344146def646e
Mohammad Jahangiry <mjahangiry75@gmail.com>
2022-07-07 13:24:42 +0430

    Implement coroutines module

---
+++ common/coroutines/src/main/java/com/sabaidea/coroutines/CoroutinesModule.kt
@@ -0,0 +1,19 @@
+package com.sabaidea.coroutines
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object CoroutinesModule {
+
+    @Provides
+    @Singleton
+    @ViewModelCoroutineDispatcher
+    fun provideViewModelCoroutineDispatcher(): CoroutineDispatcher = Dispatchers.Main
+}
---
+++ common/coroutines/src/main/java/com/sabaidea/coroutines/ViewModelCoroutineDispatcher.kt
@@ -0,0 +1,7 @@
+package com.sabaidea.coroutines
+
+import javax.inject.Qualifier
+
+@Qualifier
+@Retention(AnnotationRetention.RUNTIME)
+annotation class ViewModelCoroutineDispatcher
